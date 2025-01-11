0211526bcb64415cf56feeef1a2c505665e04484
NirCohen1 <nircohen133@gmail.com>
2025-01-12 00:38:16 +0200

    File uploader, styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import './styles/index.css';
+import './styles/flags.css';
+
+
+import TemplateDemo from './FileUploader';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PrimeReactProvider>
+      <TemplateDemo />
+      </PrimeReactProvider>
+      
     </div>
   );
 }
